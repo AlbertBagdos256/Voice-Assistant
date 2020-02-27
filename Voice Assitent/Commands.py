a70e77aad70bc5d5b9ca6fb000454b4c746b2403
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2020-02-27 16:10:48 +0500

    Update Commands.py

--- Voice Assitent/Commands.py
+++ Voice Assitent/Commands.py
@@ -1,5 +1,7 @@
 #List of Assistent's Commands
 Commands_Time = ['what time is now?','time','what time is it now']
-Commands_System = ['open telegram','open browser','open cmd','open honeypot','create folder','create file','launch music','open visual studio']
+Commands_System = ['open telegram','open browser','open cmd','open honeypot','create folder','create file','open visual studio']
 Commands_Wikipedia = ['what is','who is','The']
+Commands_Boring_Stuff = ['i am boring','tell me a joke','make me funny']
+Jokes = ['My author didnt teach me a jokes hahahaha','Why should i hire a software engineer if i can just copy and paste code from Stack Overflow']
 Phrases_after_Actions = ['as you are wish', 'sir','here we go, sir','okay!','ofcourse','no problems, sir!']
